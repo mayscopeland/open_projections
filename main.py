1be6603d47825ecaeb54fab17c8c4774c7c6f14c
mayscopeland <4038051+mayscopeland@users.noreply.github.com>
2022-03-28 09:52:40 -0500

    Fix daily stat build for SQLite

--- main.py
+++ main.py
@@ -17,20 +17,17 @@ def index():
 @app.route('/stats/<int:year>', methods=['PUT'])
 def put_year_stats(year):
     build_logs.build_yearly_gamelogs(year)
-    build_logs.combine_gamelogs()
     return "Built logs for " + str(year)
 
 @app.route('/stats/<int:year>/<int:month>', methods=['PUT'])
 def put_month_stats(year, month):
     build_logs.build_monthly_gamelogs(year, month)
-    build_logs.combine_gamelogs()
     return "Built logs for " + str(year) + "-" + str(month)
 
 @app.route('/stats/<int:year>/<int:month>/<int:day>', methods=['PUT'])
 def put_day_stats(year, month, day):
     date_string = str(year) + "-" + str(month).zfill(2) + "-" + str(day).zfill(2)
     build_logs.build_daily_gamelogs(date_string)
-    build_logs.combine_gamelogs()
     return "Built logs for " + date_string
 
 @app.route('/projections/<string:date_string>', methods=['PUT'])
